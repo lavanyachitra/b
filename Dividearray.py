c3c2355b120f85524a2cc6584f234627357c0c4d
lavanyachitra <lavanya19399@gmail.com>
2019-06-08 14:33:39 +0530

    Update Dividearray.py

--- Dividearray.py
+++ Dividearray.py
@@ -1,26 +1,13 @@
-def findSplit(ar,n):
-    leftSum=0
-    for i in range(0,n):
-        leftSum +=ar[i]
-        rightSum=0
-    for j in range(i+1,n):
-         rightSum +=ar[j]
-    if(leftSum == rightSum):
-         return i+1
-    return-1
- def printTwo(ar,n):
-      splitp=findSplit(ar,n)
-      if(splitp ==-1 orsplitp == n):
-            print("not possible)
-            return
-      for i  in range(0,n):
-         if(splitp==i):
-            print("")
-         print(str(ar[i])+' ',end= ' ')
- ar=[1,2,3,4,5,6]
- n=len(ar)
- printTwo(ar,n)
-         
-         
+a=int(input())
+l=[int(x) for x in input().split()]
+av=int(a/2)
+l1=l[:av]
+l2=l[av::]
+n1=sum(l1)//len(l1)
+n2=sum(l2)//len(l2)
+if n1==n2:
+    print("yes")
+else:
+    print("no")
       
     
