3e1eda87791d8f1f0d7c9c338010efe0286e756e
lavanyachitra <lavanya19399@gmail.com>
2019-06-22 15:51:53 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -1,18 +1,7 @@
-import java.*;
-import java.util.Arrays;
-class Largest
-static int findMax(int ar[],int n)
-{
-Arrays.sort(ar);
-int num =ar[0];
-for(int i=n-1;i>=0;i--)
-{
-num = num*10+ar[i];
-}
-return num;
-}
-public static void main(String[] args)
-int ar[]={1,2,3,4,5,0};
-int n =ar.length;
-System.out.println(findMax(ar,n));
-}
+zx=int(input())-1
+p=list(map(int,input().split()))
+s=0
+p.sort()
+for i in range(zx,-1,-1):
+	s=s*10+p[i]
+print(s)
