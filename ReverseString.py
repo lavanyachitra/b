91f95bb5e3e2849d27c6960d4385cea59d4e14da
lavanyachitra <lavanya19399@gmail.com>
2019-06-22 15:58:58 +0530

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -1,8 +1,6 @@
-def reverse(sentence):
-     words=sentence.split(" ")
-     newwords=[word[::-1]for word in words]
-     newsentence =" ",join(newwords)
-     return newsentence
- sentence="Hello World"
- print(reverse(sentence))
+ba=input().split()
+ln=[]
+for i in ba:
+  ln.append(i[::-1])
+print(*ln,sep=' ')
      
