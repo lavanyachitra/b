a185c8b07bbff7c687eae7e17d9c79621f439e0c
lavanyachitra <lavanya19399@gmail.com>
2019-07-01 15:41:49 +0530

    Update Repeating.py

--- Repeating.py
+++ Repeating.py
@@ -1,13 +1,12 @@
-def findRepeating(ar,low,high):
-    iflow>high:
-        return-1
-mid=(low+hyigh)/2
-if(ar[mid]!=mid+1):
-   if(mid>0 and ar[mid]==ar[mid-1]):
-       return mid
-    return findRepeating(ar,mid+1,high)
-ar=[1,2,3,4,5]
-n=len(ar)
-index=findReapting(ar,0,n-1)
-if(index is not-1):
-   print ar[index]
+from collections import Counter
+z=int(input())
+x=list(map(int,input().split()))
+y=Counter(x)
+list=[]
+for a in y.items():
+  if(a[1] != 1):
+    print(a[0],end = " ")
+for b in y.items():
+  list.append(b[1])
+if(max(list) == 1):
+  print("unique")
