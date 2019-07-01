aa7fa0fb54e2ecef4fed702b1dcdc5ff6779ad6b
lavanyachitra <lavanya19399@gmail.com>
2019-07-01 15:32:53 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,17 +1,7 @@
-def toString(list):
-   return ' '.join(list)
-def permute(a,l,d):
-   if l == d:
-       print toString(a)
-   else:
-       for i in xrange(l,d+1):
-           a[l],a[i] = a[i],a[l]
-           permute(a,1+1,d)
-           a[l],a[i]=a[i],a[l]
- string="ABC"
- n=len(string)
- a=list(string)
- permute(a,0,n-1)
-    
+from itertools import permutations
+n=input()
+l=permutations(n)
+for j in list(l):
+    print("".join(j))
    
    
